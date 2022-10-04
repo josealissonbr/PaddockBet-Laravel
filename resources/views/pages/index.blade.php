e96dd16cf2e8a80c189c8e5da8b4a3ef9857d12b
José Alisson <98172458+josealissonbr@users.noreply.github.com>
2022-10-04 17:37:46 -0300

    remove slide

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -117,112 +117,111 @@
 
 
      <!-- banner begin -->
-     <div class="banner-slider owl-carousel owl-theme">
+     
         <div class="banner banner-2 slide-1">
 
             <div class="container">
                 <div class="banner-content" >
                    <div class="payment-history" id="eventos-index" style="padding-top:60px;">
-        <div class="container" style="background: white">
-            <div class="row" style="background: white">
-                <div class="col-xl-8 col-lg-8">
-                    <div class="user-panel-title">
-                        <h3>Próximas provas</h3>
+                    <div class="container" style="background: white">
+                        <div class="row" style="background: white">
+                            <div class="col-xl-8 col-lg-8">
+                                <div class="user-panel-title">
+                                    <h3>Próximas provas</h3>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
-            </div>
-
-        </div>
-    </div>
         <!-- payment history end -->
 
-    @foreach (\App\Models\Eventos::where('situacao', 1)->limit(5)->get() as $evento)
+                @foreach (\App\Models\Eventos::where('situacao', 1)->limit(5)->get() as $evento)
 
-        @if (\App\Models\Provas::where('idEvento', $evento->idEvento)->where('situacao', 1)->count() == 0)
-            @continue
-        @endif
+                    @if (\App\Models\Provas::where('idEvento', $evento->idEvento)->where('situacao', 1)->count() == 0)
+                        @continue
+                    @endif
 
-        <!-- bet-slip begin -->
-        <div class="bet-slip" style="padding: 0px;background: white" >
-            <div class="container">
-                <div class="row">
-                    <div class="col-xl-8 col-lg-8">
-                        <div class="bet-slip-content">
-                            <div class="box-heading">
-                                <h4>{{$evento->nomeEvento}}</h4>
-                                <h4></h4>
-                            </div>
-                            <div>
-                                @foreach (\App\Models\Provas::where('idEvento', $evento->idEvento)->where('situacao', 1)->orderBy('dataProva', 'ASC')->get() as $prova)
-                                <div class="different-bet">
-                                    <div class="single-bet">
-                                        <div class="left-side">
-                                            <span class="bet-place">{{Carbon\Carbon::parse($prova->dataProva)->format('d/m/Y h:i')}}  - {{$prova->nomeProva}}</span>
-                                            <span class="bet-price">{{$prova->altura}}</span>
+                    <!-- bet-slip begin -->
+                    <div class="bet-slip" style="padding: 0px;background: white" >
+                        <div class="container">
+                            <div class="row">
+                                <div class="col-xl-8 col-lg-8">
+                                    <div class="bet-slip-content">
+                                        <div class="box-heading">
+                                            <h4>{{$evento->nomeEvento}}</h4>
+                                            <h4></h4>
                                         </div>
-                                        <div class="right-side">
-                                            <div class="buttons">
-                                                <a href="{{route('dashboard.provas.palpite', $prova->idProva)}}" class="buy-ticket bet-btn bet-btn-dark-light"><i class="fa fa-plus"></i> Fazer Palpite</a>
+                                        <div>
+                                            @foreach (\App\Models\Provas::where('idEvento', $evento->idEvento)->where('situacao', 1)->orderBy('dataProva', 'ASC')->get() as $prova)
+                                            <div class="different-bet">
+                                                <div class="single-bet">
+                                                    <div class="left-side">
+                                                        <span class="bet-place">{{Carbon\Carbon::parse($prova->dataProva)->format('d/m/Y h:i')}}  - {{$prova->nomeProva}}</span>
+                                                        <span class="bet-price">{{$prova->altura}}</span>
+                                                    </div>
+                                                    <div class="right-side">
+                                                        <div class="buttons">
+                                                            <a href="{{route('dashboard.provas.palpite', $prova->idProva)}}" class="buy-ticket bet-btn bet-btn-dark-light"><i class="fa fa-plus"></i> Fazer Palpite</a>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            @endforeach
+                                        </div>
+
+                                        <div>
+
+                                        </div>
+                                    </div>
+                                </div>
+
+                                @php
+                                    $proxProva = \App\Models\Provas::where('idEvento', $evento->idEvento)->orderBy('dataProva', 'asc')->get()->first();
+                                @endphp
+
+                                    <div class="col-xl-4 col-lg-4">
+                                        <div class="bet-slip-sidebar">
+                                            <h4 class="title">Próx Prova: {{$proxProva->nomeProva}}</h4>
+                                            <div class="sidebar-content">
+                                                <ul>
+                                                    <li>
+                                                        <span class="title">Início</span>
+                                                        <span class="number">{{Carbon\Carbon::parse($proxProva->dataProva)->format('d/m/Y')}}</span>
+                                                    </li>
+                                                    <li>
+                                                        <span class="title">Valor Por Palpite</span>
+                                                        <span class="number">R$ {{number_format($proxProva->valor, 2, ',')}}<span>
+                                                    </li>
+                                                </ul>
+                                                <div class="total-returns">
+                                                    <span class="text">
+                                                        Prêmio Total
+                                                    </span>
+                                                    <span class="number">
+                                                        R$ {{number_format($proxProva->saldoAcumulado, 2, ",", " ")}}
+                                                    </span>
+                                                </div>
+
                                             </div>
                                         </div>
                                     </div>
-                                </div>
-                                @endforeach
-                            </div>
-
-                            <div>
-
                             </div>
                         </div>
                     </div>
 
-                    @php
-                        $proxProva = \App\Models\Provas::where('idEvento', $evento->idEvento)->orderBy('dataProva', 'asc')->get()->first();
-                    @endphp
-
-                        <div class="col-xl-4 col-lg-4">
-                            <div class="bet-slip-sidebar">
-                                <h4 class="title">Próx Prova: {{$proxProva->nomeProva}}</h4>
-                                <div class="sidebar-content">
-                                    <ul>
-                                        <li>
-                                            <span class="title">Início</span>
-                                            <span class="number">{{Carbon\Carbon::parse($proxProva->dataProva)->format('d/m/Y')}}</span>
-                                        </li>
-                                        <li>
-                                            <span class="title">Valor Por Palpite</span>
-                                            <span class="number">R$ {{number_format($proxProva->valor, 2, ',')}}<span>
-                                        </li>
-                                    </ul>
-                                    <div class="total-returns">
-                                        <span class="text">
-                                            Prêmio Total
-                                        </span>
-                                        <span class="number">
-                                            R$ {{number_format($proxProva->saldoAcumulado, 2, ",", " ")}}
-                                        </span>
-                                    </div>
-
-                                </div>
-                            </div>
-                        </div>
-                </div>
-            </div>
-        </div>
-
-        <hr>
-        <!-- bet-slip end -->
-    @endforeach
+                    <hr>
+                    <!-- bet-slip end -->
+                @endforeach
                 </div>
             </div>
 
         </div>
 
-    </div>
+
     <!-- banner end -->
 
 
-   
+
 
         <!-- footer begin -->
         <div class="footer" id="contact">
