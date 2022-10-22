80b35f9dac9daf1f53d32bd159e23cd51bbcd17d
José Alisson <98172458+josealissonbr@users.noreply.github.com>
2022-10-22 19:52:31 -0300

    Update novoDeposito.js

--- public/assets/js/custom/novoDeposito.js
+++ public/assets/js/custom/novoDeposito.js
@@ -94,7 +94,7 @@ function initFunction(){
 
 function copyPix(){
     $('#copy-btn').popover('show');
-    ClipboardHelper.copyText(this.value);
+    ClipboardHelper.copyText($('#linhaPix').val());
 
     setTimeout(function(){
         $('#copy-btn').popover('hide');
