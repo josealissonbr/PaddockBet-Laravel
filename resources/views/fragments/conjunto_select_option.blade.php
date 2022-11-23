7ccba2fc158bb3ac2245ded5dbf02f3342d88e64
José Alisson <98172458+josealissonbr@users.noreply.github.com>
2022-11-23 20:18:31 -0300

    update Odds

--- resources/views/fragments/conjunto_select_option.blade.php
+++ resources/views/fragments/conjunto_select_option.blade.php
@@ -1,4 +1,4 @@
 <option value="" disabled>-- Selecione --</option>
 @foreach ($prova->conjuntos as $conjunto)
-<option value="{{$conjunto->idProvaConjunto}}">{{$conjunto->nomeConjunto}} ({{number_format(($prova->saldoAcumulado + ($prova->valor * $qtdCotas)) / ((\App\Models\Apostas::where('idProva', $prova->idProva)->where('ConjuntoEscolhido', $conjunto->idProvaConjunto)->sum('qtdeCotas') + $qtdCotas) * $prova->valor) + 1, 2, '.')}}x)</option>
+<option value="{{$conjunto->idProvaConjunto}}">{{$conjunto->nomeConjunto}} ({{number_format(($prova->saldoAcumulado + ($prova->valor * $qtdCotas)) / ((\App\Models\Apostas::where('idProva', $prova->idProva)->where('ConjuntoEscolhido', $conjunto->idProvaConjunto)->sum('qtdeCotas') + $qtdCotas) * $prova->valor), 2, '.')}}x)</option>
 @endforeach
