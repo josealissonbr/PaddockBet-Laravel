5b767cf8fd5795106fa78db7b43be3d6736cfc85
José Alisson <98172458+josealissonbr@users.noreply.github.com>
2023-01-27 00:32:57 -0300

    Update DepositoController.php

--- app/Http/Controllers/DepositoController.php
+++ app/Http/Controllers/DepositoController.php
@@ -183,7 +183,9 @@ class DepositoController extends Controller
         $response = $obApiPix->createCob(Str::random(26),$request);
 
         if(!isset($response['location'])){
-            echo "retorno1 falso";
+            echo "<pre>";
+            print_r($gerencianet);
+            echo "</pre>";
             return false;
         }
 
